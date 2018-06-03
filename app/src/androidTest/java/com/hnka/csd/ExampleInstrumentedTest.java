7d7a37a5f4d08f6b9a891e5eb5a2e34a845e629e
hnka <mchma@cin.ufpe.br>
2018-06-03 20:27:39 -0300

    initial project with tabs

---
+++ app/src/androidTest/java/com/hnka/csd/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.hnka.csd;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.hnka.csd", appContext.getPackageName());
+    }
+}
