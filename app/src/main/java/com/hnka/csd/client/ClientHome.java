ec727c4d4d7b2f728c2961d2a3d8fbef83b2929c
hnka <mchma@cin.ufpe.br>
2018-06-25 22:42:31 -0300

    creating client home

---
+++ app/src/main/java/com/hnka/csd/client/ClientHome.java
@@ -0,0 +1,12 @@
+package com.hnka.csd.client;
+
+public class ClientHome {
+
+    private ClientHome() {
+    }
+
+    public void getFeed() {
+        String url = ClientFactory.HOST + "/api/profile/" + id;
+    }
+
+}
