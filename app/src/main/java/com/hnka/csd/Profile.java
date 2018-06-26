6a5256f3bc442ec919323eb29ce5b97a0db83825
ahlp <ahlp>
2018-06-25 22:27:48 -0300

    adjusting profile activity

--- app/src/main/java/com/hnka/csd/Profile.java
+++ app/src/main/java/com/hnka/csd/Profile.java
@@ -1,11 +1,18 @@
 package com.hnka.csd;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hnka.csd.client.ClientFactory;
+import com.hnka.csd.client.ClientLogin;
+import com.hnka.csd.client.ClientProfile;
 
 public class Profile extends AppCompatActivity {
 
@@ -26,4 +33,41 @@ public class Profile extends AppCompatActivity {
         });
     }
 
+    public void CreateUpdateProfile(View v) {
+        ClientProfile clientProfile = ClientFactory.getClientProfileInstance();
+        ClientLogin clientLogin = ClientFactory.getClientLoginInstance();
+
+        boolean isCreatingProfile = true;
+
+        String userName = ((EditText)findViewById(R.id.ProfileUsername)).getText().toString();
+        String password = ((EditText)findViewById(R.id.ProfilePassword)).getText().toString();
+
+        String birthday = ((EditText)findViewById(R.id.ProfileBirthday)).getText().toString();
+        String bios = ((EditText)findViewById(R.id.ProfileBios)).getText().toString();
+
+        if (isCreatingProfile){
+            // TODO: adjust requests
+            clientLogin.register(userName, password);
+            clientLogin.login(userName, password);
+            clientProfile.createProfile(userName, birthday, "",bios);
+        } else {
+            clientProfile.updateProfile(userName, birthday, "",bios);
+        }
+    }
+
+    private void ProfileCreateWithSuccess() {
+        Toast.makeText(getApplicationContext(), "Welcome to CSD!", Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(this, HomeFragment.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
+
+    private void ProfileUpdatedWithSuccess() {
+        Toast.makeText(getApplicationContext(), "Profile Update with success", Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(this, HomeFragment.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/hnka/csd/client/ClientFactory.java
+++ app/src/main/java/com/hnka/csd/client/ClientFactory.java
@@ -6,6 +6,7 @@ public class ClientFactory {
     public static final String HOST = "localhost:30412";
 
     private static ClientLogin clientLogin;
+    private static ClientProfile clientProfile;
 
     public static ClientLogin getClientLoginInstance() {
         if (ClientFactory.clientLogin == null){
@@ -14,4 +15,13 @@ public class ClientFactory {
 
         return ClientFactory.clientLogin;
     }
+
+    public static ClientProfile getClientProfileInstance() {
+        if (ClientFactory.clientProfile == null){
+            ClientFactory.clientProfile = new ClientProfile();
+        }
+
+        return ClientFactory.clientProfile;
+    }
+
 }
