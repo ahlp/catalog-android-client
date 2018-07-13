46f0305a8e99efa83a03de17650ad606f331bcf4
ahlp <ahlp>
2018-07-12 22:16:22 -0300

    adding create series

--- app/src/main/java/com/hnka/csd/CreateSerieActivity.java
+++ app/src/main/java/com/hnka/csd/CreateSerieActivity.java
@@ -1,20 +1,21 @@
 package com.hnka.csd;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.hnka.csd.client.ClientFactory;
 import com.hnka.csd.client.ClientSerie;
-
-import java.util.HashMap;
 
 public class CreateSerieActivity extends AppCompatActivity {
 
@@ -39,9 +40,17 @@ public class CreateSerieActivity extends AppCompatActivity {
     public void CreateSerie(View v) {
         ClientSerie clientSerie = ClientFactory.getClientSerieInstance(getApplicationContext());
 
-        HashMap<String, String> params = new HashMap<>();
+        String poster = ((EditText)findViewById(R.id.CreateSeriePoster)).getText().toString();
+        String title = ((EditText)findViewById(R.id.CreateSerieTitle)).getText().toString();
+        String about = ((EditText)findViewById(R.id.CreateSerieAbout)).getText().toString();
+        String launch = ((EditText)findViewById(R.id.CreateSerieLaunchDate)).getText().toString();
+        String seasons = ((EditText)findViewById(R.id.createSerieSeasons)).getText().toString();
+        String episodes = ((EditText)findViewById(R.id.CreateSerieEpisodes)).getText().toString();
 
-        clientSerie.createSerie(params,
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String token = sharedPref.getString(getString(R.string.token_pref_key), "");
+
+        clientSerie.createSerie(title, launch, poster, about, seasons, episodes, token,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
--- app/src/main/java/com/hnka/csd/client/ClientSerie.java
+++ app/src/main/java/com/hnka/csd/client/ClientSerie.java
@@ -2,6 +2,7 @@ package com.hnka.csd.client;
 
 import android.content.Context;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -35,10 +36,52 @@ public class ClientSerie {
         this.queue.add(stringRequest);
     }
 
-    public void createSerie(Map<String, String> params, Response.Listener<String> listener,
+    public void createSerie(final String title, final String launch_date,
+                            final String posterLink, final String about,
+                            final String number_of_seasons, final String episodes_per_season,
+                            final String token,
+                            Response.Listener<String> listener,
                             Response.ErrorListener errorListener)
     {
-        this.requestSeries(params, Request.Method.POST, "", listener, errorListener);
+//        {
+//            "serie": {
+//            "title": "Hello My Twenties",
+//                    "launch_date": "22/08/2016",
+//                    "about": "A slice-of-life story about five college students who connect over the growing pains in their youth.",
+//                    "number_of_seasons": 2,
+//                    "episodes_per_season": 12
+//             }
+//        }
+        String url = ClientFactory.HOST + "/csd/api/series/";
+
+        // POST
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, listener, errorListener) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("Content-Type","application/json");
+                params.put("token", token);
+                return params;
+            }
+            @Override
+            public byte[] getBody()
+            {
+
+                String body = "{\"profile\":{";
+
+                body = body + "\"title\":\""+ title + "\",";
+                body = body + "\"launch_date\":\""+ launch_date + "\",";
+                body = body + "\"posterLink\":\""+ posterLink + "\",";
+                body = body + "\"about\":\""+ about + "\",";
+                body = body + "\"number_of_seasons\":\"" + number_of_seasons + "\",";
+                body = body + "\"episodes_per_season\":\"" + episodes_per_season + "\"";
+
+                body = body + "}}";
+                return body.getBytes();
+            }
+        };
+
+        this.queue.add(stringRequest);
     }
 
     public void updateSerie(Map<String, String> params, String id,
